49ee631d219a1b46716de21eaafb37421d555efb
Umer Hassan <umerhassan94@gmail.com>
2015-07-23 15:04:48 +0500

    Homeshopping.pk mobile categories crawler with mondogb storage

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
 /**
  * Created by Umer on 7/22/2015.
  */
+
+//    Requiring the modules
 var request = require('request');
 var cheerio = require('cheerio');
+var db = require('monk')('umer:1234@ds061318.mongolab.com:61318/dropped');
 
-var APPLE_ID=227;
-var SAMSUNG_ID=30;
+// Homeshopping producs IDs
+
+var APPLE_ID = 227;
+var SAMSUNG_ID = 30;
+var HTC_ID = 29;
+var QMOBILE_ID=269;
+
+//Collection names
+
+var samsung = "samsungcollection";
+var apple = "applecollection";
+var qmobile = "qmobilecollection";
+var htc = "htccollection"
 
 
+
+
+// arrays to store fetched data of the products
 
 var samsungStorage = [];
 var appleStorage = [];
+var htcStorage = [];
+var qmobileStorage = [];
 
+getProducts(SAMSUNG_ID, samsung);
+getProducts(APPLE_ID, apple);
+getProducts(HTC_ID, qmobile);
+getProducts(QMOBILE_ID, htc);
 
+function getProducts(brand_id, storage) {
+    var page = 1;
 
-getProducts(SAMSUNG_ID, samsungStorage);
+    var collection =  db.get(storage);
+   collection.remove({});
 
-getProducts(APPLE_ID,appleStorage);
-
-
-
-
-
-
-function getProducts(brand_id, storage){
-storage = [];
-    var page=1;
-
-   var clear= setInterval(function(){
-        request("http://homeshopping.pk/categories/Mobile-Phones-Price-Pakistan/?page=" + page.toString() + "&sort=newest&brandid="+brand_id+"&AjaxRequest=1", function (error, response, html) {
-            if(error){
+    var clear = setInterval(function () {
+        request("http://homeshopping.pk/categories/Mobile-Phones-Price-Pakistan/?page=" + page.toString() + "&sort=newest&brandid=" + brand_id + "&AjaxRequest=1", function (error, response, html) {
+            if (error) {
                 console.log('error');
-            }else
-            if (!error && response.statusCode == 200) {
+            } else if (!error && response.statusCode == 200) {
                 console.log("request executed");
 
-                if(html!="") {
+                if (html != "") {
                     console.log("entered else");
                     page++;
                     var $ = cheerio.load(html);
@@ -52,23 +66,25 @@ storage = [];
                         name = elem.find('.ProductDetails a').text();
                         price = elem.find('.ProductPriceRating em').text();
 
-                        var obj = {
+                        //var obj = {
+                        //    productName: name,
+                        //    productPrice: price,
+                        //    productImg: imgLink,
+                        //    productLink: link
+                        //};
+                        //storage.push(obj);
+                        collection.insert({
                             productName: name,
                             productPrice: price,
                             productImg: imgLink,
                             productLink: link
-                        };
-                        storage.push(obj);
-
-
-
-
+                        });
                     });
 
-                }else{
+                } else {
                     console.log("exiting...");
                     console.log(storage);
-                   clearInterval(clear);
+                    clearInterval(clear);
                 }
 
 
@@ -78,9 +94,7 @@ storage = [];
         });
 
 
-    }, 5000)
-
-
+    }, 10000);
 }
 
 
